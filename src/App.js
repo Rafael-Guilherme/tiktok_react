49662d17d41257f21296f37b9daba9ad046ab64e
Rafael <rafael_guilherme@hotmai.com>
2023-04-03 10:45:55 -0300

    final commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import { collection, getDocs } from 'firebase/firestore/lite';
 
 function App() {
 
+  let maxHeight
+  if(window.innerHeight <= 800) {
+    maxHeight = window.innerHeight
+  }
+
   const [videos, setVideos] = useState([])
 
   async function getVideos() {
@@ -21,12 +26,13 @@ function App() {
   },[])
 
   return (
-    <div className="App">
+    <div className="App" style={{ maxHeight: maxHeight + "px" }}>
       <div className="app__videos">
         
         { videos.map((item) => {
           return (
           <Video 
+            key={item.index}
             likes={item.likes}
             messages={item.messages}
             shares={item.shares}
